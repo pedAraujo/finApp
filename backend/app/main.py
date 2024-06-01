dd901dc9cb0c78543b2711d71732f4ee98004f2e
Pedro <p3d.araujo@gmail.com>
2024-05-31 21:46:03 -0300

    basic fastAPI and env setup

---
+++ backend/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+from .logger import logger
+
+app = FastAPI()
+logger.info("App started")
+
+
+@app.get("/")
+async def index() -> dict:
+    logger.info("Request to index route")
+    return {"message": "Hello, world!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
